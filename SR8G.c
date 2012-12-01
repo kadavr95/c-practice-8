f834e3095fd66c0238028aa734a0ea55656f36aa
Yaskovich Dmitry <kadavr95@outlook.com>
2012-12-01 03:42:00 +0300

    -

--- SR8G.c
+++ SR8G.c
@@ -1,106 +1,167 @@
 #include <stdio.h>
+#include <limits.h>
+#include <math.h>
+#define BASE 5
 
-int input(long double *a,long double *b,long double *c);
-int idb(long int number,int base,long int *result);
-int fdb(long double number,int base,long double *result);
-int ibd(long int number,int base,long int *result);
-int fbd(long double number,int base,long double *result);
-int output(long double c);
+long int BASEto10(long int a);
+long int toBASE(long int a10,long int *z);
+long int sum(long int x,long int y,long int *z);
+long int multiply(long int x,long int y,long int *z);
+long int power(long int x,long int y,long int *z);
 
-int main(void)
+int main(void) {
+ long int x, y,z=0,r=0,u,uu=0;
+ int ch=0;
+ char op;
+ printf("Enter expression: ");
+ scanf("%ld%c%ld", &x,&op,&y);
+ if (op=='+')
+ {
+   ch=sum(x,y,&z);
+ }
+ if (op=='-')
+ {
+   y*=-1;
+   ch=sum(x,y,&z);
+ }
+ if (op=='*')
+ {
+   ch=multiply(x,y,&z);
+ }
+ if (op=='^')
+ {
+   ch=power(x,y,&z);
+ }
+ if (ch==1)
+  printf("Z: %ld\n", z);
+ else
+ printf("Result:ERROR: Limits overflow\n");
+ z=0;
+ ch=1;
+ //r=sum(sum(power(y,3,&z),-(power(sum(y,-x,&z),2,&z)),&z),x,&z) ;
+ ch*=sum(y,-x,&z);
+ u=z;
+ z=0;
+ ch*=power(u,2,&z);
+ u=z;
+ ch*=power(y,3,&uu);
+ z=0;
+ ch*=sum(uu,(-u),&z);
+ //ch*=(power(y,3,&z),(-(u)),&z);
+ u=z;
+ z=0;
+ ch*=sum(u,x,&z);
+ printf("Result: %ld\n", z);
+ fflush(stdin);
+ getchar();
+ return 0;
+}
+long int BASEto10(long int a) {
+ int k=1;
+ long int a10=0;
+ while (a) {
+  a10 += k*(a%10);
+  k *= BASE;
+  a /= 10;
+ }
+ return a10;
+}
+
+long int toBASE(long int a10,long int *z)
 {
-  long double a,b,c,z;
-  int check=1;
-  while (1)
+ int k=1;
+ if ((a10>=-4687499) && (a10<=4687499))
+ {
+ while (a10)
+ {
+  *z += k*(a10%BASE);
+  k *= 10;
+  a10 /= BASE;
+ }
+ return 1;
+ }
+ else
+ return 0;
+}
+long int sum(long int x,long int y,long int *z)
+{
+int t1=1,t2=1;
+long int w,v;
+  x=BASEto10(x);
+  y=BASEto10(y);
+  if (y>0)
   {
-  input(&a,&b,&c);
-  output(a);
+   if ((LONG_MAX-y)>x)
+   {
+	 w=x+y;
+   }
+   else
+	 t1=0;
   }
+  else
+  if ((LONG_MIN-y)<x)
+   {
+	 w=x+y;
+   }
+   else
+	 t1=0;
+  v=*z;
+  t2=toBASE(w,&v);
+  *z=v;
+  return t1*t2;
 }
-
-int input(long double *a,long double *b, long double *c)
+long int multiply(long int x,long int y,long int *z)
 {
-  long double aa,bb,cc;
-  char oper,t1,t2;
-  printf("Taskset 8. Yaskovich Dmitry T01-01c\n");
-  printf("Enter expression you want to calculate\n");
-  scanf("%llf%s %llf", &aa, &oper, &bb);
-  if (oper=='b')
+int t1=1,t2=1;
+long int w,v;
+  x=BASEto10(x);
+  y=BASEto10(y);
+  if (((y>0)&&(x>0))||((y<0)&&(x<0)))
   {
-  scanf("%s %s %llf", &t1, &t2, &cc);
+   if ((LONG_MAX/y)>x)
+   {
+	 w=x*y;
+   }
+   else
+	 t1=0;
   }
-  *a=aa;
-  *b=bb;
-  *c=cc;
+  else
+  if ((LONG_MIN/y)<x)
+   {
+	 w=x*y;
+   }
+   else
+	 t1=0;
+  v=*z;
+  t2=toBASE(w,&v);
+  *z=v;
+  return t1*t2;
 }
-
-int output(long double c)
+long int power(long int x,long int y,long int *z)
 {
-  printf("Result: %.18llf\n",c);
-  printf("Dimini Inc.\n");
-  printf("For progressive future\n");
-  printf("(c)2009-2012\n");
+int t1=1,t2=1;
+long int w,v;
+  x=BASEto10(x);
+  y=BASEto10(y);
+  if ((x<0) &&(y%2==1))
+  {
+   if ((-pow(LONG_MAX,1.0/y)<x))
+   {
+	 w=pow(x,y);
+   }
+   else
+	 t1=0;
+  }
+  else
+  if (pow(LONG_MAX,1.0/y)>x)
+   {
+	 w=pow(x,y);
+   }
+   else
+	 t1=0;
+  v=*z;
+  t2=toBASE(w,&v);
+  *z=v;
+  return t1*t2;
 }
 
-
-
-
-/*
-	#include <stdio.h>
-	#include <limits.h>
-    #define BASE 5
-
-    long int BASEto10(long int a);
-    long int toBASE(long int a10,long int *a);
-
-    int main(void) {
-     long int x, y, a=0;
-     int ch=0;
-	 printf("Enter number and base of it(5 or 10): ");
-	 scanf("%ld %d", &x, &ch);
-     if (ch==10)
-     {
-       y = toBASE(x, &a);
-       if (y==0)
-    	 printf("%ld base 10 to base 5: a=%ld\n",x, a);
-       else
-    	 printf("%ld base 10 to base 5: ERROR: Limits overflow",x, a);
-     }
-     else
-     {
-       y=BASEto10(x);
-	   printf("%ld base 5 to base 10: a=%ld\n",x, y);
-     }
-     fflush(stdin);
-     getchar();
-     return 0;
-    }
-    long int BASEto10(long int a) {
-     int k=1;
-     long int a10=0;
-     while (a) {
-      a10 += k*(a%10);
-      k *= BASE;
-      a /= 10;
-     }
-     return a10;
-    }
-
-    long int toBASE(long int a10,long int *a)
-    {
-     int k=1;
-     if ((a10>=-4687499) && (a10<=4687499))
-     {
-     while (a10)
-     {
-      *a += k*(a10%BASE);
-      k *= 10;
-      a10 /= BASE;
-     }
-     return 0;
-     }
-     else
-     return 1;
-	}
-
-*/
