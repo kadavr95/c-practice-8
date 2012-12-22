c40e914b310ed4d122887c1ed6889a4e74618cfe
Yaskovich Dmitry <kadavr95@outlook.com>
2012-12-22 13:58:00 +0300

    added comments

--- SR8G.c
+++ SR8G.c
@@ -1,309 +1,207 @@
-#include <stdio.h>
-#include <limits.h>
+#include <stdio.h>//подключение библиотек ввода/вывода и математических вычислений
 #include <math.h>
-#define BASE 5
-#define LIMIT 2144444444
+#define LIMIT 2144444444//определение предела допустимых чисел
 
-long int sum(long int x,long int y,long int *z);
+long int sum(long int x,long int y,long int *z);//прототипы функций
 long int subtract(long int x,long int y,long int *z);
 long int multiply(long int x,long int y,long int *z);
 long int power(long int x,long int y,long int *z);
 
-int main(void) {
- long int x, y,z=0,r=0,u,uu=0;
+int main(void)//главная функция
+{
+ long int x, y,z=0,r=0,u,uu=0;//определение переменных
  int ch=0;
  char op;
- printf("Enter expression: ");
+ printf("Enter expression: ");//вывод запроса и считывание значений
  scanf("%ld%c%ld", &x,&op,&y);
- if ((abs(x)<=LIMIT)&&(abs(y)<=LIMIT))
+ if ((abs(x)<=LIMIT)&&(abs(y)<=LIMIT))//числа в пределах?
  {
- if (op=='+')
- {
+  if (op=='+')//используемые функции сами по себе
    ch=sum(x,y,&z);
- }
- if (op=='-')
- {
-   //y*=-1;
+  if (op=='-')
    ch=subtract(x,y,&z);
- }
- if (op=='*')
- {
+  if (op=='*')
    ch=multiply(x,y,&z);
- }
- if (op=='^')
- {
+  if (op=='^')
    ch=power(x,y,&z);
+  if (ch==1)//вычисления в пределах?
+   printf("Z: %ld\n", z);//вывод результата какой-то функции
+  else
+   printf("Result:ERROR: Limits overflow\n");//вывод ошибки
+  z=0;
+  ch=1;
+  ch*=subtract(y,x,&z);//минус в скобках
+  u=z;
+  z=0;
+  ch*=power(u,2,&z);//скобки в квадрате
+  u=z;
+  ch*=power(y,3,&uu);//число в кубе
+  z=0;
+  ch*=subtract(uu,(u),&z);//число минус скобки
+  u=z;
+  z=0;
+  ch*=sum(u,x,&z);//итог
+  if (ch==1)//вычисления в пределах
+   printf("Result: %ld\n", z);//вывод результата
+  else
+   printf("Result:ERROR: Limits overflow\n");//вывод ошибки
  }
- if (ch==1)
-  printf("Z: %ld\n", z);
  else
- printf("Result:ERROR: Limits overflow\n");
- z=0;
- ch=1;
- //r=sum(sum(power(y,3,&z),-(power(sum(y,-x,&z),2,&z)),&z),x,&z) ;
- ch*=subtract(y,x,&z);
- u=z;
- z=0;
- ch*=power(u,2,&z);
- u=z;
- ch*=power(y,3,&uu);
- z=0;
- ch*=subtract(uu,(u),&z);
- //ch*=(power(y,3,&z),(-(u)),&z);
- u=z;
- z=0;
- ch*=sum(u,x,&z);
- printf("Result: %ld\n", z);
- }
- else
- printf("Result:ERROR: Limits overflow\n");
- fflush(stdin);
+  printf("Result:ERROR: Limits overflow\n");//вывод ошибки
+ fflush(stdin);//ожидание нажатия клавиши Enter
  getchar();
  return 0;
 }
 
-long int sum(long int x,long int y,long int *z)
+long int sum(long int x,long int y,long int *z)//сложение
 {
-int i,t1=1,t2=1,xs=1,ys=1;
-long int xx[22],yy[22],zz[22];
-long int w,v,xt,yt;
-if (x<0) {
+ int i,t1=1,t2=1,xs=1,ys=1;//определение переменных
+ long int xx[22],yy[22],zz[22],chk;
+ long int w,v,xt,yt;
+ if (x<0)//сохранение знаков
   xs=-1;
-}
-if (y<0) {
+ if (y<0)
   ys=-1;
-}
-for (i = 0; i <=20 ; i++) {
+ for (i = 0; i <=20 ; i++)//заполнение массивов нулями
+ {
   xx[i]=0;
   yy[i]=0;
   zz[i]=0;
-}
-if ((xs*ys)==1) {
-for (i = 1; i <= 20; i++) {
-	 xx[i]=abs(x%10);
-	 x=x/10;
-	 yy[i]=abs(y%10);
-	 y=y/10;
-	 }
-	 for (i = 1; i <= 20; i++) {
-
-		 zz[i]=xx[i]+yy[i]+zz[i];
-	 if (zz[i]>=5) {
-	  zz[i]=zz[i]-5;
-	  zz[i+1]++;
-	 }
-	 }
-	 for (i = 1; i <= 20; i++) {
-	  *z=*z+zz[i]*pow(10,i-1);
-	 }
-	 if (z>LIMIT) {
-	   t1=0 ;
-	 }
- // if ((x>=0)&&(y>=0)) {
-
- // }
-  if ((xs<0)&&(ys<0)) {
-	*z=*z*(-1);
+ }
+ if ((xs*ys)==1)//числа одного знака
+ {
+  chk=0;//проверка вычислений в пределах
+  subtract(LIMIT,abs(x),&chk);
+  if (chk<abs(y))
+  {
+   t1=0 ;
   }
-}
-else
-{
- xt=x;
- yt=y;
-   if (abs(x)>=abs(y)) {
-	x=abs(x);
-	y=abs(y);
-	for (i = 1; i <= 20; i++) {
-	 xx[i]=x%10;
-	 x=x/10;
-	 yy[i]=y%10;
-	 y=y/10;
-	 }
-	 for (i = 1; i <= 20; i++) {
-
-		 zz[i]=zz[i]+xx[i]-yy[i];
-	 if (zz[i]<0) {
-	  zz[i]=zz[i]+5;
-	  zz[i+1]--;
-	 }
-	 }
-	 for (i = 1; i <= 20; i++) {
-	  *z=*z+zz[i]*pow(10,i-1);
-	 }
-	 if (z>LIMIT) {
-	   t1=0 ;
-	 }
+  for (i = 1; i <= 20; i++)//разбивка чисел на символы
+  {
+   xx[i]=abs(x%10);
+   x=x/10;
+   yy[i]=abs(y%10);
+   y=y/10;
+  }
+  for (i = 1; i <= 20; i++)//вычисление результата в массиве
+  {
+   zz[i]=xx[i]+yy[i]+zz[i];
+   if (zz[i]>=5)//проверка выхода из системы счисления
+   {
+	zz[i]=zz[i]-5;
+	zz[i+1]++;
    }
-	else
+  }
+  for (i = 1; i <= 20; i++)//перевод из массива в число
+   *z=*z+zz[i]*pow(10,i-1);
+  if ((xs<0)&&(ys<0))//корректировка знака
+   *z=*z*(-1);
+ }
+ else//числа разных знаков
+ {
+  xt=x;
+  yt=y;
+  if (abs(x)>=abs(y))//первое больше по модулю
+  {
+   x=abs(x);
+   y=abs(y);
+   for (i = 1; i <= 20; i++)//разбивка чисел на символы
+   {
+	xx[i]=x%10;
+	x=x/10;
+	yy[i]=y%10;
+	y=y/10;
+   }
+   for (i = 1; i <= 20; i++)//вычисление результата в массиве
+   {
+	zz[i]=zz[i]+xx[i]-yy[i];
+	if (zz[i]<0)//проверка нехватки числа в своём разряде
 	{
-	x=abs(x);
-	y=abs(y);
-	for (i = 1; i <= 20; i++) {
-	 xx[i]=x%10;
-	 x=x/10;
-	 yy[i]=y%10;
-	 y=y/10;
-	 }
-	 for (i = 1; i <= 20; i++) {
-
-		 zz[i]=zz[i]+yy[i]-xx[i];
-	 if (zz[i]<0) {
-	  zz[i]=zz[i]+5;
-	  zz[i+1]--;
-	 }
-	 }
-	 for (i = 1; i <= 20; i++) {
-	  *z=*z+zz[i]*pow(10,i-1);
-	 }
-	 if (z>LIMIT) {
-	   t1=0 ;
-	 }
-	 *z=*z*(-1);
+	 zz[i]=zz[i]+5;
+	 zz[i+1]--;
 	}
-   if ((xt<0)&&(yt>=0)) {
-	*z=*z*(-1);
+   }
+   for (i = 1; i <= 20; i++)//перевод из массива в число
+	*z=*z+zz[i]*pow(10,i-1);
   }
-}
-  //x=BASEto10(x);
- // y=BASEto10(y);
- /* if (y>0)
+  else//второе больше по модулю
   {
-   if ((LONG_MAX-y)>x)
+   x=abs(x);
+   y=abs(y);
+   for (i = 1; i <= 20; i++)//разбивка чисел на символы
    {
-	 w=x+y;
+	xx[i]=x%10;
+	x=x/10;
+	yy[i]=y%10;
+	y=y/10;
    }
-   else
-	 t1=0;
+   for (i = 1; i <= 20; i++)//вычисление результата в массиве
+   {
+	zz[i]=zz[i]+yy[i]-xx[i];
+	if (zz[i]<0)//проверка нехватки числа в своём разряде
+	{
+	 zz[i]=zz[i]+5;
+	 zz[i+1]--;
+	}
+   }
+   for (i = 1; i <= 20; i++)//перевод из массива в число
+	*z=*z+zz[i]*pow(10,i-1);
+   *z=*z*(-1);//корректировка знака
   }
-  else
-  if ((LONG_MIN-y)<x)
-   {
-	 w=x+y;
-   }
-   else
-	 t1=0;
-  v=*z;
-//  t2=toBASE(w,&v);
-  *z=v; */
-  return t1*t2;
+  if ((xt<0)&&(yt>=0))
+   *z=*z*(-1);//корректировка знака
+ }
+ return t1*t2;//возврат ошибок
 }
-long int subtract(long int x,long int y,long int *z)
+
+long int subtract(long int x,long int y,long int *z)//вычитание
 {
- long int ztmp;
+ long int ztmp=*z;//определение переменных
  int t1=1;
- ztmp=*z;
- y=-y;
- t1=sum(x,y,&ztmp);
- *z=ztmp;
- return t1;
-}
-long int multiply(long int x,long int y,long int *z)
-{
-int t1=1,t2=1,xs=1,ys=1;
-long int ztmp,zres,yrs;
-if (x<0) {
-  xs=-1;
-}
-if (y<0) {
-  ys=-1;
-}
-x=abs(x);
-y=abs(y);
-ztmp=*z;
-zres=*z;
-yrs=y;
- while (y>0)
- {
- ztmp=zres;
- zres=0;
- t1=sum(ztmp,x,&zres);
- y=yrs;
- yrs=0;
- t2=sum(y,-1,&yrs);
- }
- if ((xs*ys)==-1) {
-  ztmp=-1;
- }
- *z=ztmp;
-return t1*t2;
-/*int t1=1,t2=1;
-long int w,v;
-  //x=BASEto10(x);
- // y=BASEto10(y);
-  if (((y>0)&&(x>0))||((y<0)&&(x<0)))
-  {
-   if ((LONG_MAX/y)>x)
-   {
-	 w=x*y;
-   }
-   else
-	 t1=0;
-  }
-  else
-  if ((LONG_MIN/y)<x)
-   {
-	 w=x*y;
-   }
-   else
-	 t1=0;
-  v=*z;
- // t2=toBASE(w,&v);
-  *z=v;
-  return t1*t2; */
-}
-long int power(long int x,long int y,long int *z)
-{
-int t1=1,t2=1,xs=1,ys=1;
-long int ztmp,zres,yrs;
-if (x<0) {
-  xs=-1;
-}
-if (y<0) {
-  ys=-1;
-}
-x=abs(x);
-y=abs(y);
-ztmp=1;
-zres=1;
-yrs=y;
- while (y>0)
- {
- ztmp=zres;
- zres=0;
- t1=multiply(ztmp,x,&zres);
- y=yrs;
- yrs=0;
- t2=sum(y,-1,&yrs);
- }
- //if ((xs*ys)==-1) {
- // ztmp=-1;
- //}
- *z=ztmp;
-return t1*t2;
-/*int t1=1,t2=1;
-long int w,v;
- // x=BASEto10(x);
- // y=BASEto10(y);
-  if ((x<0) &&(y%2==1))
-  {
-   if ((-pow(LONG_MAX,1.0/y)<x))
-   {
-	 w=pow(x,y);
-   }
-   else
-	 t1=0;
-  }
-  else
-  if (pow(LONG_MAX,1.0/y)>x)
-   {
-	 w=pow(x,y);
-   }
-   else
-	 t1=0;
-  v=*z;
- // t2=toBASE(w,&v);
-  *z=v;
-  return t1*t2;   */
+ y=-y;//смена знака второй переменной
+ t1=t1*sum(x,y,&ztmp);//вызов сложения
+ *z=ztmp;//результат
+ return t1;//возврат ошибок
 }
 
+long int multiply(long int x,long int y,long int *z)//умножение
+{
+ int t1=1,t2=1,xs=1,ys=1;//определение переменных
+ long int ztmp=*z,zres=*z,yrs=y;
+ if (x<0)//сохраняем знаки переменных
+  xs=-1;
+ if (y<0)
+  ys=-1;
+ x=abs(x);//берём модули
+ y=abs(y);
+ while (y>1)//сложение пока подходит множитель
+ {
+  ztmp=zres;
+  zres=0;
+  t1=t1*sum(ztmp,x,&zres);//вызов сложения
+  y=yrs;
+  yrs=0;
+  t2=t2*sum(y,-1,&yrs);//уменьшение множителя
+ }
+ if ((xs*ys)==-1)//корректировка знака
+  zres=-1*zres;
+ *z=zres;//результат
+ return t1*t2;//возврат ошибок
+}
 
+long int power(long int x,long int y,long int *z)//степень
+{
+ int t1=1,t2=1,xs=1,ys=1;//определение переменных
+ long int ztmp=x,zres=x,yrs=(y-1);
+ while (y>1)//умножение пока подходит степень
+ {
+  ztmp=zres;
+  zres=0;
+  t1=t1*multiply(ztmp,x,&zres);//вызов умножения
+  y=yrs;
+  yrs=0;
+  t2=t2*sum(y,-1,&yrs);//уменьшение степени
+ }
+ *z=zres;//результата
+ return t1*t2;//возврат ошибок
+}
